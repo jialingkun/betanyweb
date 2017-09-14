fc3546cee99d2b5e523ed34086d0afe47e48b497
Bent Hartz <Bent Hartz>
2017-09-14 16:42:17 +0700

    Initial commit

---
+++ footer.php
@@ -0,0 +1,17 @@
+<!-- content closing div -->
+</div>
+<!-- content closing div -->
+
+
+<div class="footer">Copyright © 2017 <a href="http://www.bethanymalang.org/" class="company-link">Grha Bethany Malang</a></div>
+
+
+
+
+
+<!-- wraper closing div -->
+</div>
+<!-- wraper closing div -->
+
+</body>
+</html>
