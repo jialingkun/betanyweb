9fc6f927cb5a9453a0ce4bc3c3b4d450b6a982d7
Bent Hartz <Bent Hartz>
2017-09-17 16:28:45 +0700

    Live Streaming Weekly Countdown

--- index.php
+++ index.php
@@ -1,36 +1,5 @@
 <?php include 'part/header.php';?>
 
-<script>
-// Set the date we're counting down to
-var countDownDate = new Date();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // Output the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-    
-    // If the count down is over, write some text 
-    if (distance < 0) {
-    	clearInterval(x);
-    	document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-}, 1000);
-</script>
-
 <style>
 
 	/*override boostrap carousel style*/
@@ -93,18 +62,185 @@ var x = setInterval(function() {
 		<span class="sr-only">Next</span>
 	</a>
 </div>
-
-<div class="timer-wrapper center">
-	<h5>Watch Our Live Services In</h5>
-	<div>0 <br>days</div>
-	<div>11 <br>hours</div>
-	<div>28 <br>mins</div>
-	<div>19 <br>secs</div>
-	<button type="button" class="btn btn-danger hide">WATCH NOW!</button>
-	<a href=" https://www.youtube.com/channel/UCDbS-b_NMp5jqZw1YfAo_vA"><button type="button" class="btn btn-primary">PREVIOUS BROADCASTS</button></a>
+<div class="responsive-timer">
+	<div class="timer-wrapper center">
+		<h5 id="timerLabel">Watch Our Live Services In</h5>
+		<div id="days">0 <br>days</div>
+		<div id="hours">11 <br>hours</div>
+		<div id="mins">28 <br>mins</div>
+		<div id="secs">19 <br>secs</div>
+		<a href="stream.php" id="streaming" class="hide"><button type="button" class="btn btn-danger">WATCH NOW!</button></a>
+		<a href=" https://www.youtube.com/channel/UCDbS-b_NMp5jqZw1YfAo_vA"><button type="button" class="btn btn-primary">PREVIOUS BROADCASTS</button></a>
+	</div>
 </div>
+<script>
+	var dueDays = [];
+	var dueHours = [];
+	var dueMinutes = [];
+	var dueTotalSeconds = [];
+	var endHours = [];
+	var endMinutes = [];
+	var endTotalSeconds = [];
+
+	//date 1
+	dueDays[0] = 6; //0 Sunday, 1 Monday, 2 Tuesday ,......, 6 Saturday
+	//17:30
+	dueHours[0] = 17; //24 Hour format, ex 1 pm = 13
+	dueMinutes[0] = 30;
+	//19:00
+	endHours[0] = 19;
+	endMinutes[0] = 0;
+
+	//date 2
+	dueDays[1] = 0; // 0 Sunday
+
+	dueHours[1] = 9;
+	dueMinutes[1] = 0;
+
+	endHours[1] = 11;
+	endMinutes[1] = 0;
+
+	//date 3
+	dueDays[2] = 0; // 0 Sunday
+
+	dueHours[2] = 16;
+	dueMinutes[2] = 30;
+
+	endHours[2] = 18;
+	endMinutes[2] = 30;
+
+	// get today date
+	var nowDate = new Date();
+	var nowTotalSeconds;
+
+	var duetimesdiff = [];
+	var endtimesdiff = [];
+
+	var duedaysdiff = [];
+	var enddaysdiff = [];
+
+	var nearestDueDay;
+	var nearestEndDay;
+	var nearestDay;
+	var nearestDueTime;
+	var nearestEndTime;
+
+	var secTime;
+	var ticker;
+
+	getSeconds();
+	
+
+	function getSeconds(){
+		nowTotalSeconds = (nowDate.getHours()*60*60)+(nowDate.getMinutes()*60)+nowDate.getSeconds();
+
+		for(i=0;i<dueDays.length;i++){
+			//find different between today date time and due date time
+			dueTotalSeconds[i] = (dueHours[i]*60*60)+(dueMinutes[i]*60);
+			duetimesdiff[i] = dueTotalSeconds[i]-nowTotalSeconds;
+			if (duetimesdiff[i]>0) {
+				duedaysdiff[i] = dueDays[i] - nowDate.getDay();
+			}else{
+				duedaysdiff[i] = dueDays[i] - nowDate.getDay()-1;
+			}
+			if (duedaysdiff[i]<0) {
+				duedaysdiff[i] += 7;
+			}
+			if (duetimesdiff[i] <= 0) { 
+				duetimesdiff[i] += (24*60*60);
+			}
+
+			//find different between today date time and end date time
+			endTotalSeconds[i] = (endHours[i]*60*60)+(endMinutes[i]*60);
+			endtimesdiff[i] = endTotalSeconds[i]-nowTotalSeconds;
+			if (endtimesdiff[i]>0) {
+				enddaysdiff[i] = dueDays[i] - nowDate.getDay();
+			}else{
+				enddaysdiff[i] = dueDays[i] - nowDate.getDay()-1;
+			}
+			if (enddaysdiff[i]<0) {
+				enddaysdiff[i] += 7;
+			}
+			if (endtimesdiff[i] <= 0) { 
+				endtimesdiff[i] += (24*60*60);
+			}
+		}
+
+		nearestDueDay = Math.min.apply(Math, duedaysdiff);
+		nearestEndDay = Math.min.apply(Math, enddaysdiff);
+
+		if (nearestDueDay<nearestEndDay) {
+			nearestDay = nearestDueDay;
+		}else{
+			nearestDay = nearestEndDay;
+		}
+
+		var tempDueTimes = [];
+		var tempEndTimes = [];
+		for(i=0;i<duedaysdiff.length;i++){
+			if (duedaysdiff[i] <= nearestDay) {
+				tempDueTimes.push(duetimesdiff[i]);
+			}
+		}
+
+		for(i=0;i<enddaysdiff.length;i++){
+			if (enddaysdiff[i] <= nearestDay) {
+				tempEndTimes.push(endtimesdiff[i]);
+			}
+		}
+
+		nearestDueTime = Math.min.apply(Math, tempDueTimes);
+		nearestEndTime = Math.min.apply(Math, tempEndTimes);
+
+		if (nearestDueTime<nearestEndTime) {
+			startTimer(false,nearestDueTime);
+		}else{
+			startTimer(true,nearestEndTime);
+		}
 
 
+		// console.log(Math.floor(nearestDueTime / (60 * 60)));
+		// console.log(Math.floor(nearestEndTime / (60 * 60)));
+	}
 
 
-<?php include 'part/footer.php';?>
+	function startTimer(isLive,secs){
+		if (isLive == true) {
+			document.getElementById("timerLabel").innerHTML = "We Are Now Live Streaming! <br> End In:";
+			document.getElementById("streaming").className = "";
+		}else{
+			document.getElementById("timerLabel").innerHTML = "Watch Our Live Services In";
+			document.getElementById("streaming").className = "hide";
+		}
+		secTime = parseInt(secs);
+		ticker = setInterval("tick()",1000);
+		tick();
+	}
+
+	function tick() {
+		var secs = secTime;
+		if (secs>0) {
+			secTime--;
+		}
+		else {
+			clearInterval(ticker);
+			getSeconds();
+		}
+
+		var hours= Math.floor(secs/3600);
+		secs %= 3600;
+		var mins = Math.floor(secs/60);
+		secs %= 60;
+
+ 		//update the time display
+ 		document.getElementById("days").innerHTML = nearestDay + "<br>days";
+ 		document.getElementById("hours").innerHTML =hours + "<br>hours";
+ 		document.getElementById("mins").innerHTML = mins + "<br>mins";
+ 		document.getElementById("secs").innerHTML = secs + "<br>secs";
+ 	}
+
+
+ </script>
+
+
+ <?php include 'part/footer.php';?>
